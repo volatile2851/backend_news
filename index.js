4de33d11c900fa782b09b539fd6fa676fb4b01f0
Harshit Singh <singhharshit984@gmail.com>
2023-10-10 01:40:07 +0530

    first commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import DefaultData from './default.js'
 const app = express();
 dotenv.config();
 app.set("trust proxy", 1)
-app.use(cors({origin : '*' , credentials : true}));
+app.use(cors({origin : 'https://65245d9730106b351dac7431--fabulous-tapioca-1868aa.netlify.app/' , credentials : true}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 app.use('/', Route);
